f88bf6cfebdfbabfa5a0206d52d0a45e0e2ac1b2
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-20 11:44:09 -0400

    made it mine

--- twosums/src/TwoSums.java
+++ twosums/src/TwoSums.java
@@ -2,22 +2,22 @@ import java.util.Arrays;
 
 public class TwoSums {
     public static void main(String[] args) {
-        int[] myArray = {3,3};
-        int target = 6;
-        System.out.println(Arrays.toString(twoSum(myArray, target)));
+        int[] myArray = {2, 8, 11, 15,9,0};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(myArray,target)));
     }
 
 
         public static int[] twoSum(int[] nums, int target) {
             for (int i = 0; i < nums.length; i++) {
-                for (int j = i + 1; j < nums.length; j++) {
-                    if (nums[j] == target - nums[i]) {
+                for (int j = 1; j < nums.length; j++) {
+                    if (nums[j] +nums[i] == target) {
                         return new int[] { i, j };
                     }
                 }
             }
             // If no valid pair is found, return an empty array instead of null
-            return new int[] {0,0};
+            return new int[] {};
         }
     }
 
