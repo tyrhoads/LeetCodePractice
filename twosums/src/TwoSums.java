e18a162217085f814688129c8c8852dded4a9bf3
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-20 10:28:42 -0400

    added

---
+++ twosums/src/TwoSums.java
@@ -0,0 +1,13 @@
+public class TwoSums
+{
+    public static void main(String[] args)
+    {
+        int[] myArray = {2,7,11,15};
+        int target = 9;
+        System.out.println(twoSum(myArray,target));
+    }
+    public static int[] twoSum(int[] nums, int target)
+    {
+            return nums;
+    }
+}
