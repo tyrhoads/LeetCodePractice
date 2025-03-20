8ec672ee559582167cf93d30331f12cc9eb41508
Tyler Rhoads <tyrhoads@gmail.com>
2025-03-20 11:42:03 -0400

    finished

--- twosums/src/TwoSums.java
+++ twosums/src/TwoSums.java
@@ -1,13 +1,23 @@
-public class TwoSums
-{
-    public static void main(String[] args)
-    {
-        int[] myArray = {2,7,11,15};
-        int target = 9;
-        System.out.println(twoSum(myArray,target));
+import java.util.Arrays;
+
+public class TwoSums {
+    public static void main(String[] args) {
+        int[] myArray = {3,3};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(myArray, target)));
     }
-    public static int[] twoSum(int[] nums, int target)
-    {
-            return nums;
+
+
+        public static int[] twoSum(int[] nums, int target) {
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[j] == target - nums[i]) {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            // If no valid pair is found, return an empty array instead of null
+            return new int[] {0,0};
+        }
     }
-}
+
